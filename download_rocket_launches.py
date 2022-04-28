3130b7ec58de0e8763710056abcedecb047d6645
maalshekto <tmartin@live.fr>
2022-04-28 14:43:20 +0200

    fix bad indentation

--- download_rocket_launches.py
+++ download_rocket_launches.py
@@ -27,17 +27,17 @@ def _get_pictures():
         launches = json.load(f)
         image_urls = [launch["image"] for launch in launches["results"]]
         for image_url in image_urls:
-        try:
-            response = requests.get(image_url)
-            image_filename = image_url.split("/")[-1]
-            target_file = f"/tmp/images/{image_filename}"
-            with open(target_file, "wb") as f:
-                f.write(response.content)
-                print(f"Downloaded {image_url} to {target_file}")
-        except requests_exceptions.MissingSchema:
-            print(f"{image_url} appears to be an invalid URL.")
-        except requests_exceptions.ConnectionError:
-            print(f"Could not connect to {image_url}.")
+            try:
+                response = requests.get(image_url)
+                image_filename = image_url.split("/")[-1]
+                target_file = f"/tmp/images/{image_filename}"
+                with open(target_file, "wb") as f:
+                    f.write(response.content)
+                    print(f"Downloaded {image_url} to {target_file}")
+            except requests_exceptions.MissingSchema:
+                print(f"{image_url} appears to be an invalid URL.")
+            except requests_exceptions.ConnectionError:
+                print(f"Could not connect to {image_url}.")
 
 get_pictures = PythonOperator( 
     task_id="get_pictures",
