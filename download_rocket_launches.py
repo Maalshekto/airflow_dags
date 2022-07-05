09a050a3a98c4a32eba16d08a1ccb1c3ee44a6da
Thomas MARTIN <tmartin@live.fr>
2022-07-05 22:58:50 +0200

    Update download_rocket_launches.py

--- download_rocket_launches.py
+++ download_rocket_launches.py
@@ -50,7 +50,6 @@ def _get_pictures():
     shell_source("/app/openrc/openrc.sh")
     print(f"OS_TENANT_NAME {os.getenv('OS_TENANT_NAME')}")
     # retrieve from Swift container
-    options = {'out_directory': '/tmp'}
     with SwiftService() as swift:
         for down_res in swift.download(container='swift_airflow_rocket_dag', objects=['launches.json'], options = options):
             if down_res['success']:
