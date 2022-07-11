e8bdb91680954754670f0cd64af7552a3de87a7f
Thomas MARTIN <tmartin@live.fr>
2022-07-11 23:33:55 +0200

    Update surprise.py

--- surprise.py
+++ surprise.py
@@ -1,4 +1,54 @@
+import airflow
+import time
+from airflow.operators.python import PythonOperator
+from airflow import DAG
 
 
 from qiskit import IBMQ, assemble, transpile
 from qiskit.circuit.random import random_circuit
+
+def _real_quantum_backend(): 
+  time.sleep(100)
+  
+def _simulator_perfect_quantum_backend():
+  time.sleep(10)
+
+def simulator_noisy_quantum_backend():
+  time.sleep(10)
+
+def _print_result():
+  time.sleep(2)
+ 
+
+dag = DAG (
+    dag_id="surprise_quantique",
+    start_date=airflow.utils.dates.days_ago(14),
+    schedule_interval=None,
+)
+
+Q1 = PythonOperator( 
+    task_id="real_quantum_backend",
+    python_callable=_real_quantum_backend, 
+    dag=dag,
+)
+
+Q2 = PythonOperator(
+    task_id="simulator_perfect_quantum_backend",
+    python_callable=_simulator_perfect_quantum_backend, 
+    dag=dag,
+)
+
+Q3 = PythonOperator(
+    task_id="simulator_noisy_quantum_backend",
+    python_callable=_simulator_noisy_quantum_backend, 
+    dag=dag,
+)
+
+res= PythonOperator(
+    task_id="simulator_noisy_quantum_backend",
+    python_callable=_print_result(), 
+    dag=dag,
+)
+
+
+[Q1, Q2, Q3] >> res
